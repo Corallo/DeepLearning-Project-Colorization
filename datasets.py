6411c912de459c3acb9465ce62e770da959d984e
Carles Balsell <carlesbalsellsrodas@hotmail.com>
2020-05-04 00:48:37 +0200

    commented encoding for future inspection

--- datasets.py
+++ datasets.py
@@ -20,15 +20,15 @@ class ImageNet(Dataset):
             for imagePath in imagePaths:
                 self.listData.append(os.path.join(totalDirPath,imagePath))
 
-        ab_bins = np.load('pts_in_hull.npy')
-        nbrs = NearestNeighbors(n_neighbors=5, algorithm='kd_tree', p=2).fit(ab_bins)
-        def soft_encode_i(x):
-            distances, indices = nbrs.kneighbors(x.reshape((1,-1)))
-            res = np.zeros(313)
-            res[indices.reshape(-1)] = softmax(np.exp(-(distances.reshape(-1)**2)/50))
-            return res
+        #ab_bins = np.load('pts_in_hull.npy')
+        #nbrs = NearestNeighbors(n_neighbors=5, algorithm='kd_tree', p=2).fit(ab_bins)
+        #def soft_encode_i(x):
+        #    distances, indices = nbrs.kneighbors(x.reshape((1,-1)))
+        #    res = np.zeros(313)
+        #    res[indices.reshape(-1)] = softmax(np.exp(-(distances.reshape(-1)**2)/50))
+        #    return res
 
-        self.soft_encode_i = soft_encode_i
+        #self.soft_encode_i = soft_encode_i
 
     def __len__(self):
 
@@ -41,6 +41,6 @@ class ImageNet(Dataset):
         image_ab = cv2.resize(inputImage, (64, 64), interpolation = cv2.INTER_AREA)[:,:,1:].astype(float) - 128.0
         image_L = self.transf(inputImage[:,:,0].astype(float))
 
-        encoded_ab = self.transf(np.apply_along_axis(self.soft_encode_i,-1,image_ab))
+        #encoded_ab = self.transf(np.apply_along_axis(self.soft_encode_i,-1,image_ab))
         
-        return image_L, encoded_ab
+        return image_L, image_ab
